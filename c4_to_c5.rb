ea750abb3ab3a2375a3c8e7aa767d2464ed5b516
Jeremy Morse <jgmorse@umich.edu>
2020-03-02 18:12:22 -0500

    All pdf and text subtypes treated as unknown chapters; subtype pg/img treated as 1/25th of an unknown chapter.

--- c4_to_c5.rb
+++ c4_to_c5.rb
@@ -15,6 +15,21 @@ def create_idno_map
   return map
 end
 
+def create_epub_map
+  map = Hash.new
+  #consume csv of monograph noid, epub noid
+  #convert to hash
+  return map
+end
+
+def get_access_type(input)
+  #consume csv of OA hebids or noids
+  #if this title is on the list
+    #return 'OA_Gold'
+  #else`
+    #return 'Controlled'
+end
+
 header_row = [
   'session',
   'institution',
@@ -32,22 +47,50 @@ header_row = [
   'parent_noid'
 ]
 
-map = create_idno_map
+idno_map = create_idno_map
+epub_map = create_epub_map
 
 CSV.open('data/output.csv', 'w') do |output|
   output << header_row
+
+  titles_seen = Hash.new(0)
 
   CSV.foreach(ARGV.shift, headers: true) do |input|
     #Expand each row into one row per hit
     hits = input['total']
     hits.to_i.times {
       row = CSV::Row.new(header_row,[])
+
+      #track which book we're counting in this period
+      idno = input['title']
+      titles_seen[ idno ]+=1
+
       row['session'] = "Migrated from DLXS stats for HELIO-3240 on #{DateTime.now} ID:#{SecureRandom.hex(10)}"
       row['institution'] = input['institution']
 
-      row['noid'] = map[ input['title'] ]
+      row['noid'] = epub_map[ idno_map[ idno ] ]
+      row['model'] = 'FileSet'
+      row['section'] = 'unknown'
+      row['section_type'] = 'Chapter'
+      row['investigation'] = 1
+      row['request'] = 1
+      row['turnaway'] = nil
+      row['access_type'] = get_access_type(input)
+      row['created_at'] = input['hitdate']
+      row['updated_at'] = input['hitdate']
+      row['press'] = 16
+      row['parent_noid'] = idno_map[ idno ]
+      row['hebid'] = idno
+      row['subtype'] = input['subtype']
 
-      output << row
+      if input['subtype'] == 'pdf'
+        output << row
+      elsif input['subtype'] == 'text'
+        output << row
+      elsif input['subtype'] == 'pg/img' && titles_seen[ idno ] % 25 == 0
+        #Effectively count each page view as 1/25th of a chapter
+        output << row
+      end
     }
   end
 end
